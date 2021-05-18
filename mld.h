bbcd826f2963c640dc51007fb157408675f4e4b7
harish-kumar-97 <harishec031@gmail.com>
2021-05-18 23:15:24 +0530

    adding data structures used for MLD tool

--- mld.h
+++ mld.h
@@ -119,4 +119,15 @@ void* xcalloc(object_db_t *object_db, char *struct_name, int units);
 
 /* APIs to register root objects */
 void mld_register_root_object(object_db_t *object_db,
-				
+				void *objptr,
+				char *struct_name,
+				unsigned int uints);
+void set_mld_object_as_global_root(object_db_t *object_db, void *obj_ptr);
+
+/* APIs for MLD Algorithm */
+void run_mld_algorithm(object_db_t *object_db);
+void report_leaked_objects(object_db_t *object_db);
+void mld_set_dynamic_object_as_root(object_db_t *object_db, void *obj_ptr);
+void mld_init_primitive_data_types_support(struct_db_t *struct_db);
+
+#endif /* __MLD__ */
